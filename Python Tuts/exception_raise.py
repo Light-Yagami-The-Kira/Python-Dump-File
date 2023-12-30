e8e77849a1ee8a5c52bc13f69d708e9d5c17639d
Ehtical-Hacker-Krishanu01 <krishanukarmakar.kk94@gmail.com>
2023-12-31 00:25:08 +0530

    raise exception

---
+++ Python Tuts/exception_raise.py
@@ -0,0 +1,17 @@
+# import re
+# a = input("What is your name: ")
+# x = re.compile(r'\d').findall(a)
+# if x != []:
+#     raise Exception("Numbers are not allowed.")
+# # print(x)
+# print(f"Hello, {a} bhai.")
+
+# try:
+#     a = int(input("Enter a number"))
+# except ValueError:
+#     raise ValueError("Bhai Number Daal.")
+
+# try:
+#     a = int(input("Enter a number: "))
+# except ValueError:
+#     print("Bhai Number Daal.")
