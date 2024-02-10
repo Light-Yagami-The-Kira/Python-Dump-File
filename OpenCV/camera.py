74a712f596502cdf1831833a751214d924afd5df
Light-Yagami-The-Kira <krishanukarmakar.kk94@gmail.com>
2024-02-10 17:16:47 +0530

    Video Capture

---
+++ OpenCV/camera.py
@@ -0,0 +1,27 @@
+import cv2
+
+# cap = cv2.VideoCapture(1)
+# cap = cv2.VideoCapture("Path/to/video")
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output_video.mp4', fourcc, 30.0, (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT))
+
+print(
+    cap.get(cv2.CAP_PROP_FRAME_WIDTH),
+    cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    )
+while cap.isOpened():
+    return_value,frame = cap.read()
+    if return_value == True:
+        out.write(frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("Window Title", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+else:
+    print("Camera Not Avaialble")
+
+cap.release()
+cv2.destroyAllWindows()
