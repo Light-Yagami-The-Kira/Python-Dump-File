510e87d13689b38f46759046ccdf85c7888ff5a7
Ehtical-Hacker-Krishanu01 <krishanukarmakar.kk94@gmail.com>
2024-02-22 07:05:05 +0530

    Camera Params

---
+++ OpenCV/camera_parameters.py
@@ -0,0 +1,37 @@
+import cv2
+
+# cap = cv2.VideoCapture(1)
+# cap = cv2.VideoCapture("Path/to/video")
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output_video.avi', fourcc, 30.0, (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT))
+
+print(
+    cap.get(cv2.CAP_PROP_FRAME_WIDTH),
+    cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    )
+
+cap.set(3, 1080)
+cap.set(4, 1080)
+
+print(
+    cap.get(cv2.CAP_PROP_FRAME_WIDTH),
+    cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    )
+
+while cap.isOpened():
+    return_value,frame = cap.read()
+    if return_value == True:
+        out.write(frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("Window Title", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+else:
+    print("There is no Camera Avaialble")
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
